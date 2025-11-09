dd93f21d296354a20f571d53ef1ada68431cd689
siddique <siddiquemd161@gmail.com>
2025-11-09 06:47:07 +0600

    Set server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require("express")
+
+const app=express()
+
+const port=process.env.PORT||3000
+
+app.get("/", (req,res)=>{
+    res.send("server is now running")
+})
+
+
+app.listen(port, ()=>{
+    console.log(`app listening on port ${port}`)
+})
